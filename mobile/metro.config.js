89af677b3e8d50dada33f2c5b2175616cbb4991f
JoaoCarlinho <JSkeete@gmail.com>
2025-11-12 15:32:50 -0600

    continue testing linkedin login

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
@@ -7,5 +8,19 @@ config.resolver.assetExts.push('wasm');
 
 // Handle expo-sqlite web compatibility
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Exclude backend directory from Metro bundler
+// This prevents Node.js code from being imported into React Native
+config.resolver.blockList = [
+  // Ignore backend directory entirely
+  /backend\/.*/,
+  // Ignore aws-lambdas directory
+  /aws-lambdas\/.*/,
+];
+
+// Set watchFolders to only include mobile directory and shared root node_modules
+config.watchFolders = [
+  path.resolve(__dirname, '..'), // Root for shared node_modules
+];
 
 module.exports = config;
